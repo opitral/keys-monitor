8bc227f3c0e25482ee84f887c0c4a5edb69b565b
Volodymyr Slobodian <opitral@proton.me>
2024-11-21 02:52:08 +0200

    add keys listener

---
+++ key_listener.py
@@ -0,0 +1,16 @@
+from pynput import keyboard
+from key_repository import KeyRepository
+from config import settings
+from logger_config import get_logger
+
+logger = get_logger(__name__, settings.LOGGING_FILE)
+repository = KeyRepository(settings.DATABASE_URL)
+
+
+def on_press(key):
+    logger.debug("Key '%s' pressed.", key)
+    repository.add_key_press(str(key))
+
+
+with keyboard.Listener(on_press=on_press) as listener:
+    listener.join()
